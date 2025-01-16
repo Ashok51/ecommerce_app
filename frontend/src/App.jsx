4a2bea846c7029af671d3903b593299266ecde95
Ashok Poudel <ashokpoudel2051@gmail.com>
2025-01-17 01:15:10 +0545

    add header and footer with some customized css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1>Welcome to React App</h1>
-      </div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to React App</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
