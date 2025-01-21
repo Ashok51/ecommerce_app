a3cebe1aece4945a3d913d4068ebef10e6af8bbe
Ashok Poudel <ashokpoudel2051@gmail.com>
2025-01-21 11:53:46 +0545

    setup backend for api

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import products from "../frontend/products.js"; // remember: esmodule call with .js extension
+
+const PORT = 5000;
+
+const app = express()
+
+app.get('/api/products', (req, res)=>{
+  res.json(products)
+})
+
+app.get('/api/productd/:id', (req, res)=>{
+  const product = products.find((p)=> p._id == req.params.id)
+  res.json(product)
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running in port: ${PORT}`)
+})
