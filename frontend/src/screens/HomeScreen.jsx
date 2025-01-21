f188b7495f090bbb61a8f5f2ab7f6352ffb8a9d4
Ashok Poudel <ashokpoudel2051@gmail.com>
2025-01-21 21:24:43 +0545

    fetch data from api endpints using hook

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../../products";
 import Product from "../components/Product";
 import axios from "axios";
 import { useEffect, useState } from "react";
