e141259eb7031456c3e6f52991bb46c88064e245
Ashok Poudel <ashokpoudel2051@gmail.com>
2025-01-27 23:51:23 +0545

    add products Api Slice for injecting the endpoint to apliSlice

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,18 @@
+// this file is to inject the endpoint to apiSlice.js file
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5, // FOR CACHING
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApiSlice
+
+// convension is use--queryname--query OR use-mutationname--mutation OR ......
