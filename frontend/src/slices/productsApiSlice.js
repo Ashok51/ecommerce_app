dadf339c2af7b6450f17d147005cb20fb15252e0
Ashok Poudel <ashokpoudel2051@gmail.com>
2025-01-28 16:21:49 +0545

    add fetching data using redux by removing react useeffect and usestate hooks

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,18 +1,18 @@
 // this file is to inject the endpoint to apiSlice.js file
 import { PRODUCTS_URL } from "../constants";
-import { apiSlice } from "./apiSlice";
+import { apiSlice } from './apiSlice';
 
-export const productsApiSlice = apiSlice.injectEndpoints({
+const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => ({
-        url: PRODUCTS_URL,
+        url: '/api/products',
       }),
       keepUnusedDataFor: 5, // FOR CACHING
     }),
   }),
 });
 
-export const { useGetProductsQuery } = productsApiSlice
+export const { useGetProductsQuery } = productsApiSlice;
 
 // convension is use--queryname--query OR use-mutationname--mutation OR ......
